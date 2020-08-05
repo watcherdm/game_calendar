e365a318e2b07d2d402b5f6b688ec50d162b7ddc
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2020-08-04 21:50:06 -0700

    initial commit

---
+++ setup_db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: 'watcher_game_calendar'
+})
+
+connection.connect()
+
+connection.query('show tables;', (err, rows, fields) => {
+  if (err) throw err
+  console.log(rows)
+})
+
+connection.end()
